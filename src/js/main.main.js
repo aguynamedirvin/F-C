e91dca52501cc91e306871a29d47e881c02137c1
Irvin Dominguez <aguynamedirvin@gmail.com>
2016-01-27 23:25:02 -0700

    Added newsletter section, added slider

--- src/js/main.main.js
+++ src/js/main.main.js
@@ -1,7 +1,36 @@
 // Initialize Slick Sliders
 $(document).ready(function(){
+
+    // Homepage Slider
     $('.featured-slider').slick({
-        'arrows': false,
-        'dots': true
+        arrows: false,
+        dots: true,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
+
+
+    // Product Slider
+    $('.product__slider').slick({
+        arrows: false,
+        dots: true,
+        asNavFor: '.product__thumbnails'
+    });
+    $('.product__thumbnails').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        asNavFor: '.product__slider',
+        focusOnSelect: true,
+        vertical: true,
+        responsive: [
+            {
+                breakpoint: 868,
+                settings: {
+                    slidesToShow: 3,
+                    vertical: false
+                }
+            }
+        ]
     });
 });
