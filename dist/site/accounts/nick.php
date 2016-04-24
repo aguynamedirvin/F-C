ae21babd83d8e29320ba537037a4e75bc4f65161
Irvin <aguynamedirvin@gmail.com>
2016-04-24 13:25:20 -0600

    Deleted homepage template. Unnecessary

--- dist/site/accounts/nick.php
+++ dist/site/accounts/nick.php
@@ -3,8 +3,11 @@
 username: nick
 email: nickmadrid@email.com
 password: >
-  $2a$10$AhycKFO.7vI.dnsvpT2x1.Gr1TBOXoPKT7.bEI7nrd7oXNAqrf.ky
-firstname: Nick Madrid
+  $2a$10$t6bnBzQ71FrOnLXL1BBOvuwdAD7ka23YtVOTiPcHZC3NhxmEL7EJ2
+firstname: Nick
 language: en
 country: united-states
 token: ef04930aaee6169efa116c066da51893
+lastname: Madrid
+role: admin
+discountcode: ""
--- dist/site/templates/homepage.php
+++
@@ -1,9 +0,0 @@
-<?php snippet('header') ?>
-
-	<main class="wrap">
-
-		<?= snippet('list.product', ['products' => $products]) ?>
-
-	</main>
-
-<?php snippet('footer') ?>
